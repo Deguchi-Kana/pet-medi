30b40575ac2c2e68ce16e299c955fdcff025231d
kdeguchi <kdeguchi@fourmix.co.jp>
2025-08-04 01:42:10 +0900

    fix: ルーティング修正

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Calendar from '../views/Calendar.vue';
 import Register from '../views/Register.vue';
-import MedicineList from "../components/MedicineList.vue";
+import Home from '../views/Home.vue';
 
 const routes = [
-    { path: '/', name: 'Home', component: MedicineList },
+    { path: '/', name: 'Home', component: Home },
     { path: '/calendar', name: 'Calendar', component: Calendar },
     { path: '/register', name: 'Register', component: Register },
 ];
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import MedicineList from "../components/MedicineList.vue";
 </script>
 
 <template>
   <h2>ペットの投薬管理アプリ</h2>
+  <medicine-list></medicine-list>
 </template>
 
 <style scoped>
